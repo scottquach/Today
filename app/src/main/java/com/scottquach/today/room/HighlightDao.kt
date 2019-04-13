f4906e541093cd58a009121526e19c99e5fc8303
Scott Quach <scottqglobal@gmail.com>
2019-04-12 17:45:29 -0700

    Fixed another localtime date issue

--- app/src/main/java/com/scottquach/today/room/HighlightDao.kt
+++ app/src/main/java/com/scottquach/today/room/HighlightDao.kt
@@ -17,12 +17,12 @@ interface HighlightDao {
     @Query("SELECT * FROM highlights WHERE goal_id = :goalId")
     fun getByGoal(goalId: Int): LiveData<List<Highlight>>
 
-    @Query("SELECT * FROM highlights WHERE date(created) = date('now', 'localtime')")
+    @Query("SELECT * FROM highlights WHERE date(created, 'localtime') = date('now', 'localtime')")
     fun getToday(): LiveData<Highlight>
 
     @Query("UPDATE highlights SET status = 'COMPLETED' WHERE id = :highlightId")
     fun completeHighlight(highlightId: Int)
-
+    
     @Insert
     fun insert(vararg highlights: Highlight)
 
